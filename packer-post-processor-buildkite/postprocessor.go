0de32fa37a80286eabcbaf26f43d997ac31a4ada
thrashr888 <thrashr888@gmail.com>
2016-04-13 12:23:21 -0700

    Add prefix option
    This also applies the golint suggestion of jobID.

--- packer-post-processor-buildkite/postprocessor.go
+++ packer-post-processor-buildkite/postprocessor.go
@@ -11,15 +11,43 @@ import (
 	buildkite "github.com/buildkite/agent/api"
 	"github.com/buildkite/agent/retry"
 
+	"github.com/mitchellh/packer/common"
+	"github.com/mitchellh/packer/helper/config"
 	"github.com/mitchellh/packer/packer"
+	"github.com/mitchellh/packer/template/interpolate"
 )
 
-type PostProcessor struct {
-	client buildkiteAgent.APIClient
-	jobId  string
+// Config options
+type Config struct {
+	common.PackerConfig `mapstructure:",squash"`
+	Prefix              string `mapstructure:"prefix"`
+	ctx                 interpolate.Context
 }
 
-func (p *PostProcessor) Configure(...interface{}) error {
+// PostProcessor master
+type PostProcessor struct {
+	config Config
+	client buildkiteAgent.APIClient
+	jobID  string
+}
+
+// Configure sets up the config options to be used later
+func (p *PostProcessor) Configure(raws ...interface{}) error {
+
+	// Configure
+	err := config.Decode(&p.config,
+		&config.DecodeOpts{
+			Interpolate: false,
+		},
+		raws...)
+	if err != nil {
+		return err
+	}
+
+	// apply default, var not required
+	if p.config.Prefix == "" {
+		p.config.Prefix = ""
+	}
 
 	agentEndpoint := os.Getenv("BUILDKITE_AGENT_ENDPOINT")
 	agentAccessToken := os.Getenv("BUILDKITE_AGENT_ACCESS_TOKEN")
@@ -28,7 +56,7 @@ func (p *PostProcessor) Configure(...interface{}) error {
 		return fmt.Errorf("must set BUILDKITE_AGENT_ENDPOINT and BUILDKITE_AGENT_ACCESS_TOKEN environment variables")
 	}
 
-	jobId := os.Getenv("BUILDKITE_JOB_ID")
+	jobID := os.Getenv("BUILDKITE_JOB_ID")
 
 	if agentEndpoint == "" || agentAccessToken == "" {
 		return fmt.Errorf("must set BUILDKITE_JOB_ID environment variable")
@@ -38,18 +66,19 @@ func (p *PostProcessor) Configure(...interface{}) error {
 		Endpoint: agentEndpoint,
 		Token:    agentAccessToken,
 	}
-	p.jobId = jobId
+	p.jobID = jobID
 
 	return nil
 }
 
+// PostProcess sends the Artifact to Buildkite
 func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (packer.Artifact, bool, error) {
 
 	client := p.client.Create()
 
 	metadata := make([]buildkite.MetaData, 0, 2)
 
-	builderId := artifact.BuilderId()
+	builderID := artifact.BuilderId()
 
 	// packer's artifact model doesn't support AMIs very well since
 	// both a region and an id need to be encoded into a single field.
@@ -57,22 +86,40 @@ func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (pac
 	// in the buildkite job.
 	// N.B. this doesn't support AMIs being created across multiple regions.
 
-	if builderId == "mitchellh.amazonebs" || builderId == "mitchellh.amazoninstance" || builderId == "mitchellh.amazonchroot" {
-		packedId := artifact.Id()
-		parts := strings.SplitN(packedId, ":", 2)
+	if builderID == "mitchellh.amazonebs" || builderID == "mitchellh.amazoninstance" || builderID == "mitchellh.amazonchroot" {
+		packedID := artifact.Id()
+		parts := strings.SplitN(packedID, ":", 2)
+
+		idKeyName := "artifact_ami_id"
+		regionKeyName := "artifact_ami_id"
+
+		// override key names with an optional prefixed version
+		if p.config.Prefix != "" {
+			idKeyName = fmt.Sprintf("%s_%s", p.config.Prefix, idKeyName)
+			regionKeyName = fmt.Sprintf("%s_%s", p.config.Prefix, regionKeyName)
+		}
+
 		metadata = append(metadata, buildkite.MetaData{
-			Key:   "artifact_ami_id",
+			Key:   idKeyName,
 			Value: parts[1],
 		})
 		metadata = append(metadata, buildkite.MetaData{
-			Key:   "artifact_ami_region",
+			Key:   regionKeyName,
 			Value: parts[0],
 		})
 	} else {
 		id := artifact.Id()
+
+		idKeyName := "artifact_id"
+
+		// override key name with an optional prefixed version
+		if p.config.Prefix != "" {
+			idKeyName = fmt.Sprintf("%s_%s", p.config.Prefix, idKeyName)
+		}
+
 		if id != "" {
 			metadata = append(metadata, buildkite.MetaData{
-				Key:   "artifact_id",
+				Key:   idKeyName,
 				Value: id,
 			})
 		}
@@ -85,7 +132,7 @@ func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (pac
 		for _, item := range metadata {
 			ui.Message(fmt.Sprintf("- %s: %s", item.Key, item.Value))
 			err := retry.Do(func(s *retry.Stats) error {
-				resp, err := client.MetaData.Set(p.jobId, &item)
+				resp, err := client.MetaData.Set(p.jobID, &item)
 				if resp != nil && (resp.StatusCode == 401 || resp.StatusCode == 404) {
 					s.Break()
 				}
